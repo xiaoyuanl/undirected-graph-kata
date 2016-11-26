4da46f546d1301ec9cf3e5c53f7f8f6720e6e76f
Graham Greenfield <grahamg@gmail.com>
2016-11-25 20:16:02 -0500

    Add some initial integration tests for the overall result.

--- src/undirected_graph/tests/test_main.py
+++ src/undirected_graph/tests/test_main.py
@@ -1,7 +1,25 @@
 import unittest
 
 from undirected_graph import main
+from undirected_graph.user_input import UserInput
+from undirected_graph.graph import Graph
 
 class TestMain(unittest.TestCase):
-    def test_mainFunctionExists(self):
-        self.assertTrue(main is not None)
+    def setUp(self):
+        graph = Graph()
+        self.user_input = UserInput(graph)
+
+    def test_providesValidFinalResult_one(self):
+        edges = '1 2 2 3 2 4 2 5 2 6 2 7'
+        result = self.user_input.interpret(edges)
+        self.assertEqual(result, 1)
+
+    def test_providesValidFinalResult_two(self):
+        edges = '1 2 2 3 3 1 4 5'
+        result = self.user_input.interpret(edges)
+        self.assertEqual(result, 2)
+
+    def test_providesValidFinalResult_four(self):
+        edges = '1 2 2 3 3 1 9 9 8 8 7 6 5 8'
+        result = self.user_input.interpret(edges)
+        self.assertEqual(result, 4)
